7eae5bca8f872850987b1185ad6162e56d3dbd25
Rachma Fadilah K <148951827+rachmafkr@users.noreply.github.com>
2023-10-30 10:11:51 +0700

    Update SI_RegPagi22166019Latihan23Perandingannilaimhs.java

--- SI_RegPagi22166019Latihan23Perandingannilaimhs.java
+++ SI_RegPagi22166019Latihan23Perandingannilaimhs.java
@@ -16,29 +16,31 @@ import java.util.Scanner;
  */
 public class SI_RegPagi22166019Latihan23Perandingannilaimhs {
     public static void main(String[] args) {
+        // Memasukkan scanner dan input
         System.out.println("======Program Terbesar dan Terkecil Nilai Mahasiswa======");
         System.out.println(); 
         Scanner scanner = new Scanner(System.in);
         Scanner input = new Scanner(System.in);
 
-        // Input the name of the officer
+        // Menginput nama petugas
         System.out.print("Masukkan nama petugas: ");
         String officerName = input.nextLine();
 
-        // Input the number of student grades
+        // Menginput berapa banyak mahasiswa yang akan dihitung nilainya
         System.out.print("Masukkan banyaknya nilai mahasiswa: ");
-        int numGrades = input.nextInt();
+        int nilaimhs = input.nextInt();
 
-        // Use an array to store the grades
-        int[] grades = new int[numGrades];
+        // Menggunakan array untuk menghitung nilai
+        int[] grades = new int[nilaimhs];
 
-        // Use a loop to input the grades and find the highest and lowest values
+        // Gunakan loop untuk memasukkan nilai dan menemukan (menentukan) nilai tertinggi dan terendah
         int highest = Integer.MIN_VALUE;
         int lowest = Integer.MAX_VALUE;
-        for (int i = 0; i < numGrades; i++) {
+        for (int i = 0; i < nilaimhs; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             grades[i] = input.nextInt();
 
+            // Masukkan kondisi
             if (grades[i] > highest) {
                 highest = grades[i];
             }
@@ -48,15 +50,16 @@ public class SI_RegPagi22166019Latihan23Perandingannilaimhs {
             }
         }
 
+        // Memasukan nilai masing-masing variabel
         System.out.println("======Hasil Nilai Mahasiswa======");
         System.out.println();
-        // Output the first four student grades
+        // Output untuk nilai mahasiswa dari yang pertama hingga selesai
         System.out.println("Nilai mahasiswa ke-1: " + grades[0]);
         System.out.println("Nilai mahasiswa ke-2: " + grades[1]);
         System.out.println("Nilai mahasiswa ke-3: " + grades[2]);
         System.out.println("Nilai mahasiswa ke-4: " + grades[3]);
         
-        // Output the name of the officer, the number of student grades, the highest grade, and the lowest grade
+        // Print nama petugas, jumlah nilai siswa, nilai tertinggi, dan nilai terendah
         System.out.println("Nilai terbesar: " + highest);
         System.out.println("Nilai terkecil: " + lowest);
         System.out.println("Petugas: " + officerName);
